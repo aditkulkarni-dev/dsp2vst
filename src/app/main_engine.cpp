845f3e0d8591db2d266d86c7e7b3cf8627b75d43
Aditya <aditya.kulkarni27@gmail.com>
2026-05-28 01:27:50 -0400

    add cmakepresets

--- src/app/main_engine.cpp
+++ src/app/main_engine.cpp
@@ -4,12 +4,11 @@
 #include "effects/FDNReverb.h"
 
 int main() {
-    AudioEngine engine("input.wav", "output.wav", 1024);
+    AudioEngine engine("C:/Users/Aditya Kulkarni/audio-engine/audio-engine/input.wav", "output.wav", 1024);
 
     // Add ur effects here
-    engine.addEffect(std::make_unique<Gain>(0.7f));
-    engine.addEffect(std::make_unique<FDNReverb>(std::vector<int>{1103, 1321, 1487, 1699, 1777, 1987, 2113, 2399}));
-
+    engine.addEffect(std::make_unique<Gain>(0.7f));   
+    engine.addEffect(std::make_unique<ffDelay>(22000, 1.0f, 44100));
     engine.run();
     return 0;
 }
--- src/effects/ffDelay.cpp
+++ src/effects/ffDelay.cpp
@@ -7,18 +7,23 @@ void ffDelay::setBufferSize(int bufferSize){
 
 void ffDelay::process(float* data, int numSamples){
     
-    
     for (int i{0}; i < numSamples; ++i){
         float input = data[i];
         
-        
+        //Read the old sample from 'delay' samples ago
         float delayed = buffer.read(delay);
  
-        
+        //Mix the dry input with the wet delayed signal
         float output = input + mix * delayed;
+        
+        //Store the current input sample into the buffer
         buffer.write(input);
+        
+        //Update the audio block array
         data[i] = output;
         
+        //Step the buffer pointer forward for the next iteration!
+        buffer.advance(); 
     }
 }
 
